93903a43c7f5b998c300c15e20c8725ead39658e
Alvaro <alvarorodmi7@gmail.com>
2024-11-01 13:54:22 +0000

    Added a lot of examples to run.py

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ print("------------------------------------")
 
 
 
-print("Breadth First Search")
+print("Depth First Search")
 start_time = time.time()
 print(search.depth_first_graph_search(ab).path())
 print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
@@ -22,7 +22,7 @@ print("------------------------------------")
 
 
 
-print("Breadth First Search")
+print("Branch and Bound")
 start_time = time.time()
 print(search.b_a_b(ab).path())
 print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
@@ -30,13 +30,157 @@ print("------------------------------------")
 
 
 
-print("Breadth First Search")
+print("Branch and Bound with subestimation")
 start_time = time.time()
 print(search.b_a_b_sub(ab).path())
 print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
 print("------------------------------------")
 
+###############################################################################
+print("###############################################################################")
 
-# Result:
-# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
-# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
+oe = search.GPSProblem('O', 'E', search.romania)
+
+print("O --> E")
+# imprimir tiempo de ejecución
+print("Breadth First Search")
+start_time = time.time()
+print(search.breadth_first_graph_search(oe).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Depth First Search")
+start_time = time.time()
+print(search.depth_first_graph_search(oe).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Branch and Bound")
+start_time = time.time()
+print(search.b_a_b(oe).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Branch and Bound with subestimation")
+start_time = time.time()
+print(search.b_a_b_sub(oe).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+###############################################################################
+print("###############################################################################")
+
+gz = search.GPSProblem('G', 'Z', search.romania)
+
+print("G --> Z")
+# imprimir tiempo de ejecución
+print("Breadth First Search")
+start_time = time.time()
+print(search.breadth_first_graph_search(gz).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Depth First Search")
+start_time = time.time()
+print(search.depth_first_graph_search(gz).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Branch and Bound")
+start_time = time.time()
+print(search.b_a_b(gz).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Branch and Bound with subestimation")
+start_time = time.time()
+print(search.b_a_b_sub(gz).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+###############################################################################
+print("###############################################################################")
+
+nd = search.GPSProblem('N', 'D', search.romania)
+
+print("N --> D")
+# imprimir tiempo de ejecución
+print("Breadth First Search")
+start_time = time.time()
+print(search.breadth_first_graph_search(nd).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Depth First Search")
+start_time = time.time()
+print(search.depth_first_graph_search(nd).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Branch and Bound")
+start_time = time.time()
+print(search.b_a_b(nd).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Branch and Bound with subestimation")
+start_time = time.time()
+print(search.b_a_b_sub(nd).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+###############################################################################
+print("###############################################################################")
+
+mf = search.GPSProblem('M', 'F', search.romania)
+
+print("M --> F")
+# imprimir tiempo de ejecución
+print("Breadth First Search")
+start_time = time.time()
+print(search.breadth_first_graph_search(mf).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Depth First Search")
+start_time = time.time()
+print(search.depth_first_graph_search(mf).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Branch and Bound")
+start_time = time.time()
+print(search.b_a_b(mf).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
+
+
+print("Branch and Bound with subestimation")
+start_time = time.time()
+print(search.b_a_b_sub(mf).path())
+print("Tiempo de ejecución:" + (str(time.time() - start_time) + " segundos"))
+print("------------------------------------")
+
